011326fc2586f21ed6744f80c58338b510477266
Collins Wegulo <wegulocollins77@gmail.com>
2024-03-07 04:22:59 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+Name=input('What is your name:')
+Age=input('What is your age:')
+Location=input('Where do you live:')
+print(f'Hello, your name is {Name}, you are {Age} years old, and you live in {Location}.')
